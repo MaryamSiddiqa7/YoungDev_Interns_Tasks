bbd12c1a3855890fa4e22aea6d3142150dcbeaa5
Maryam Siddiqa <maryamsiddiqa.227@gmail.com>
2025-07-11 22:12:54 +0500

    Delete ecommerce_project directory

--- ecommerce_project/shop/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-from django.shortcuts import render
-from .models import Product
-
-def home(request):
-    products = Product.objects.all()
-    return render(request, 'shop/home.html', {'products': products})
